42e93c16f1f7e053d826c18497bf7a4d67393631
Thiago Gomes Verissimo <thiago.verissimo@usp.br>
2018-09-20 16:37:45 -0300

    rascunhos da reunião

---
+++ app/Http/Controllers/AdminController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Site;
+use GuzzleHttp\Client;
+
+class AdminController extends Controller
+{
+    public function listaSites()
+    {
+        $client = new Client([
+             'base_uri' => 'http://aegir.fflch.usp.br/',
+        ]);
+        $res = $client->request('GET','/aegir/saas/site.json', ['query' => ['api-key' => 'GKm5xUygoJK3LicKZCDVMQ']]);
+        $sites_aegir = json_decode($res->getBody());
+        //dd($sites_aegir);
+
+        $dnszone = env('DNSZONE');
+        $sites = Site::all();
+        return view('admin/lista-sites', compact('sites','dnszone','sites_aegir'));
+    }
+}
