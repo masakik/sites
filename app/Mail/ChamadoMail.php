98ac73469da2170e611842b6f8f6b19022ee6a0e
Thiago Gomes Verissimo <verissimotgv@gmail.com>
2019-08-16 21:48:50 -0300

    Update ChamadoMail.php

--- app/Mail/ChamadoMail.php
+++ app/Mail/ChamadoMail.php
@@ -34,9 +34,18 @@ class ChamadoMail extends Mailable
      */
     public function build()
     {
+        $emails = [config('sites.email_principal'),$this->user->email];
+        // Responsável pelo site
+        $codpes = $this->chamado->site->owner;
+        $owner = User::where('codpes', $codpes)->first();
+        if ($owner) {
+            $emails[] = $user->email;
+        }
+        $emails = array_unique($emails);
+        
         return $this->view('emails.chamado')
                     ->from(config('sites.email_principal'))
-                    ->to([config('sites.email_principal'),$this->user->email])
+                    ->to($emails)
                     ->subject("Novo chamado para o site: {$this->chamado->site->dominio}" . config('sites.dnszone'));
     }
 }
