28430f87e0504a92c54e96d1fb73cd15b295a211
Thiago Gomes Verissimo <verissimotgv@gmail.com>
2020-08-24 11:40:42 -0300

    corigindo emails

--- resources/views/emails/chamado.blade.php
+++ resources/views/emails/chamado.blade.php
@@ -2,8 +2,8 @@ Novo chamado para o site {{ $chamado->site->dominio.config('sites.dnszone') }}
 
 <div>
 Número USP: {{ $user->codpes }} <br>
-Nome: {{ $user->name }} 
-Número do chamado: {{$chamado->site->id}}/{{$chamado->id}}
+Nome: {{ $user->name }}   <br>
+Número do chamado: {{$chamado->site->id}}/{{$chamado->id}}  <br>
 Chamado: <a href="{{config('app.url')}}/{{$chamado->site->id}}/{{$chamado->id}}">
   {{config('app.url')}}/{{$chamado->site->id}}/{{$chamado->id}}
 </a>
@@ -12,9 +12,8 @@ Chamado: <a href="{{config('app.url')}}/{{$chamado->site->id}}/{{$chamado->id}}"
 <br>
 
 <b>Tipo:</b> {{ $chamado->tipo }}
-<div>
 <b>Chamado:</b> {!! $chamado->descricao !!}
-</div>
+
 
 Mensagem automática do sistema de gestão de sites: {{ config('app.url') }}
 
--- resources/views/emails/comentario.blade.php
+++ resources/views/emails/comentario.blade.php
@@ -3,18 +3,18 @@ para o site {{ $comentario->chamado->site->dominio.config('sites.dnszone') }}
 
 <div>
 Número USP: {{ $user->codpes }} <br>
-Nome: {{ $user->name }} 
-Número do chamado: {{$comentario->chamado->site->id}}/{{$comentario->chamado->id}}
+Nome: {{ $user->name }}  <br>
+Número do chamado: {{$comentario->chamado->site->id}}/{{$comentario->chamado->id}}  <br>
 Chamado: <a href="{{config('app.url')}}/{{$comentario->chamado->site->id}}/{{$comentario->chamado->id}}">
     {{config('app.url')}}/{{$comentario->chamado->site->id}}/{{$comentario->chamado->id}}
 </a>
+</div>
 
 <br>
 
 <b>Status:</b> {{ $comentario->chamado->status }}
-<div>
 <b>Comentário:</b> {!! $comentario->comentario !!}
-</div>
+
 
 Mensagem automática do sistema de gestão de sites: {{ config('app.url') }}
 
