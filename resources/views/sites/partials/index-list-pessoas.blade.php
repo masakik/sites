11c46daf31a39e612378dd9f402ac89078f853e8
Masaki Kawabata Neto <kawabata@usp.br>
2022-08-11 12:18:56 -0300

    mostrando novamente administradores mesmo que vazio

--- resources/views/sites/partials/index-list-pessoas.blade.php
+++ resources/views/sites/partials/index-list-pessoas.blade.php
@@ -1,26 +1,25 @@
 <div>
-  <b>Responsável:</b> {{ $site->owner }} - {{ \Uspdev\Replicado\Pessoa::dump($site->owner)['nompes'] }} -
-  {{ \Uspdev\Replicado\Pessoa::email($site->owner) }}
+  <b>Responsável:</b> {{ $site->owner }}
+  - {{ \Uspdev\Replicado\Pessoa::dump($site->owner)['nompes'] ?? 'NA' }}
+  - {{ \Uspdev\Replicado\Pessoa::email($site->owner) }}
 
   @can('sites.update', $site)
-  <a href="sites/{{ $site->id }}/changeowner" class="btn btn-sm text-warning" title="Mudar responsável">
-    <i class="fas fa-exchange-alt"></i>
-  </a>
-@endcan
+    <a href="sites/{{ $site->id }}/changeowner" class="btn btn-sm btn-outline-warning" title="Mudar responsável">
+      <i class="fas fa-exchange-alt"></i>
+    </a>
+  @endcan
 </div>
 
-@if (!empty($site->numeros_usp))
-  <li class="list-group-item">
-    <b>Administradores:</b>
+<li class="list-group-item">
+  <b>Administradores:</b>
 
-    @can('sites.update', $site)
-      <a href="sites/{{ $site->id }}/novoadmin" title="Adicionar administrador" class="btn btn-sm text-primary">
-        <i class="fas fa-user-plus"></i>
-      </a>
-    @endcan
+  @can('sites.update', $site)
+    <a href="sites/{{ $site->id }}/novoadmin" title="Adicionar administrador" class="btn btn-sm text-primary">
+      <i class="fas fa-user-plus"></i>
+    </a>
+  @endcan
 
-    @foreach (explode(',', $site->numeros_usp) as $numero_usp)
-      @include('sites.partials.list-administrador')
-    @endforeach
-  </li>
-@endif
+  @foreach (explode(',', $site->numeros_usp) as $numero_usp)
+    @include('sites.partials.list-administrador')
+  @endforeach
+</li>
