3c307737ca3cfcff7829ff85f907ae6a536e9533
Masaki Kawabata Neto <kawabata@usp.br>
2022-08-16 16:01:45 -0300

    adicionado url de tutoriais

--- config/sites.php
+++ config/sites.php
@@ -1,9 +1,11 @@
 <?php
 
 return [
-    'dnszone' => env('DNSZONE', false),
+    'dnszone' => env('DNSZONE', null),
+    'tutoriaisUrl' =>env('TUTORIAIS_URL', null),
     'unidades_usp' => env('UNIDADES_USP',false),
     'deploy_secret_key' => env('DEPLOY_SECRET_KEY', false),
     'subdominio' => env('HABILITAR_SUBDOMINIO', false),
+    'chamados' => env('CHAMADOS', 'local'),
     'siteManager' => env('SITE_MANAGER', 'aegir'),
 ];
