85f89017b8a479d2950b8d54cc9c6e9e837590de
Masaki Kawabata Neto <kawabata@usp.br>
2022-08-16 16:04:27 -0300

    colocando chamado no menu dinâmico

--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -2,8 +2,9 @@
 
 namespace App\Providers;
 
-use Illuminate\Support\Facades\Event;
 use Illuminate\Foundation\Support\Providers\EventServiceProvider as ServiceProvider;
+use Illuminate\Support\Facades\Event;
+use Uspdev\UspTheme\Events\UspThemeParseKey;
 
 class EventServiceProvider extends ServiceProvider
 {
@@ -27,6 +28,18 @@ class EventServiceProvider extends ServiceProvider
     {
         parent::boot();
 
-        //
+        if (config('sites.chamados') != 'none') {
+            Event::listen(function (UspThemeParseKey $event) {
+                if (isset($event->item['key']) && ($event->item['key'] == 'chamados')) {
+                    $event->item = [
+                        'text' => '<span class="text-danger">Chamados</span>',
+                        'url' => 'chamados',
+                        // 'can'  => 'admin'
+                    ];
+                }
+                return $event->item;
+            });
+        }
+
     }
 }
--- config/laravel-usp-theme.php
+++ config/laravel-usp-theme.php
@@ -1,42 +1,40 @@
 <?php
 
-$avisos =  [
+$avisos = [
     [
         'text' => 'Listar',
-        'url'  => 'avisos',
+        'url' => 'avisos',
     ],
     [
         'text' => 'Cadastrar',
-        'url'  => 'avisos/create',
-        'can'     => 'admin',
+        'url' => 'avisos/create',
+        'can' => 'admin',
     ],
 ];
 
 $menu = [
     [
         'text' => 'Solicitar um Site',
-        'url'  => 'sites/create',
-        'can'  => 'sites.create'
+        'url' => 'sites/create',
+        'can' => 'sites.create',
     ],
     [
         'text' => 'Meus Sites',
-        'url'  => 'sites',
-        'can'  => 'sites.create'
+        'url' => 'sites',
+        'can' => 'sites.create',
     ],
     [
-        'text'    => '<span class="text-danger">Avisos</span>',
+        'text' => '<span class="text-danger">Avisos</span>',
         'submenu' => $avisos,
-        'can'     => 'admin',
+        'can' => 'admin',
     ],
     [
-        'text' => '<span class="text-danger">Chamados</span>',
-        'url'  => 'chamados',
-        'can'  => 'admin'
+        'key' => 'chamados',
     ],
     [
         'text' => '<span class="text-danger">Emails</span>',
-        'url'  => 'emails',
-        'can'  => 'admin'
+        'url' => 'emails',
+        'can' => 'admin',
     ],
     [
         # este item de menu será substituido no momento da renderização
@@ -49,15 +47,19 @@ $right_menu = [
         // menu utilizado para views da biblioteca senhaunica-socialite.
         'key' => 'senhaunica-socialite',
     ],
+    [
+        'text' => '<span class="text-danger" title="COnfigurações da aplicação"><i class="fas fa-cog"></i></span>',
+        'url' => 'settings',
+        'can' => 'admin',
+    ],
 ];
-
 
 return [
     # valor default para a tag title, dentro da section title.
     # valor pode ser substituido pela aplicação.
     'title' => config('app.name'),
 
-    # USP_THEME_SKIN deve ser colocado no .env da aplicação 
+    # USP_THEME_SKIN deve ser colocado no .env da aplicação
     'skin' => env('USP_THEME_SKIN', 'uspdev'),
 
     # chave da sessão. Troque em caso de colisão com outra variável de sessão.
