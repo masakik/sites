7265c750940df2706930f9215cd04ce9342ce243
Thiago Gomes Verissimo <verissimotgv@gmail.com>
2022-08-29 20:22:31 -0300

    Update EmailController.php

--- app/Http/Controllers/EmailController.php
+++ app/Http/Controllers/EmailController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Site;
+use App\Models\User;
 use Uspdev\Replicado\Pessoa;
 
 class EmailController extends Controller
@@ -15,11 +16,16 @@ class EmailController extends Controller
         $owners = Site::select('owner')->get()->toArray();
         $owners = array_unique(array_column($owners,'owner'));
         $owners_emails = [];
+        
+        $owners_emails = User::whereIn('codpes', $owners)->pluck('email')->toArray();
+        
+        /*
         foreach($owners as $codpes){
             $email = Pessoa::email($codpes);
             if(!empty($email))
                 array_push($owners_emails,$email);
         }
+        */
 
         $admins = Site::select('numeros_usp')->get()->toArray();
         $admins = array_column($admins,'numeros_usp');
@@ -29,12 +35,15 @@ class EmailController extends Controller
         $admins = array_filter($admins);
         $admins = array_unique($admins);
         
-        $admins_emails = [];
+        $admins_emails = User::whereIn('codpes', $admins)->pluck('email')->toArray();
+        
+        /*
         foreach($admins as $codpes){
             $email = Pessoa::email($codpes);
             if(!empty($email))
                 array_push($admins_emails,$email);
         }
+        */
 
         return view('emails.export')->with([
             'owners_emails' => $owners_emails,
