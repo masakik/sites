258fbaf9fb8d97d163157a5f856bb66bc45afa99
Masaki K Neto <kawabata@usp.br>
2023-05-17 08:40:15 -0300

    adicionando texto de help

--- resources/views/components/input/text.blade.php
+++ resources/views/components/input/text.blade.php
@@ -4,14 +4,17 @@
     'type' => 'text',
     'value' => '',
     'placeholder' => '',
+    'id' => 'text' . $label . $name,
+    'help' => '',
 ])
 
 <div class="form-group">
-  <label for="exampleFormControlInput1">{{ $label }}</label>
-  <input type=" {{ $type }}" 
-  class="form-control" 
-  name="{{ $name }}"
-  id="exampleFormControlInput1" 
-  placeholder=""
-  value="{{ $value }}">
+  <label for="{{ $id }}">{{ $label }}</label>
+
+  <input type=" {{ $type }}" class="form-control" name="{{ $name }}" id="{{ $id }}"
+    value="{{ $value }}" />
+
+  @if ($help)
+    <small class="form-text text-muted">{{ $help }}</small>
+  @endif
 </div>
