e33f38a5f2063cfc69e4b94fd170dcc4c64e11d8
Masaki K Neto <kawabata@usp.br>
2023-05-23 16:26:40 -0300

    typo

--- resources/views/sites/show/card-pessoas.blade.php
+++ resources/views/sites/show/card-pessoas.blade.php
@@ -1,13 +1,14 @@
 <div class="card mt-3">
   <div class="card-header py-1">
     <i class="fas fa-users"></i> Pessoas
-    @includeWhen(['sites.update', $site],'sites.partials.pessoas-add-admin')
+    @includeWhen(['sites.update', $site], 'sites.partials.pessoas-add-admin')
   </div>
   <div class="card-body py-1">
     <div>
       <b>Responsável:</b> {{ $site->owner }} - {{ $site->ownerName }} - {{ $site->ownerEmail }}
       @includeWhen(Gate::check('sites.update', $site), 'sites.partials.pessoas-change-owner')
-
+    </div>
+    <div class="ml-3">
       @foreach (explode(',', $site->numeros_usp) as $numero_usp)
         @include('sites.partials.list-administrador')
       @endforeach
